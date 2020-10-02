de063e0070b740d7cb42fc5aeecdc5201214c64c
kiyoung85 <kiyoung85@gmail.com>
2020-10-03 00:22:10 +0900

    실전 예제 - 3 다양한 연관관계 매핑

---
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,20 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Delivery {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String city;
+    private String street;
+    private String zipcode;
+
+    @Enumerated(EnumType.STRING)
+    private DeliveryStatus status;
+
+    @OneToOne(mappedBy = "delivery")
+    private Order order;
+}
