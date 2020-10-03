ae124ff9f6a31630deb1a5c569f850d5b2700438
kiyoung85 <kiyoung85@gmail.com>
2020-10-03 18:19:00 +0900

    프록시

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,13 +1,11 @@
 package jpabook.jpashop;
 
 import jpabook.jpashop.domain.Book;
-import jpabook.jpashop.domain.Member;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.time.LocalDateTime;
 
 public class JpaMain {
 
