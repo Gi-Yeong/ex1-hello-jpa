1aaf309796b02685a1bef9f570b2b301a9ca3d01
kiyoung85 <kiyoung85@gmail.com>
2020-10-04 18:39:11 +0900

    객체지향 쿼리 언어 소개(JPQL)

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,9 +1,12 @@
 package jpabook.jpashop;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import jpabook.jpashop.domain.Member;
+
+import javax.persistence.*;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
 
 public class JpaMain {
 
@@ -16,6 +19,22 @@ public class JpaMain {
         tx.begin();
 
         try {
+
+            List<Member> resultList = em.createQuery(
+                    "select m from Member m where m.name like '%kim%'", Member.class
+            ).getResultList();
+
+            //Criteria
+            CriteriaBuilder cb = em.getCriteriaBuilder();
+            CriteriaQuery<Member> query = cb.createQuery(Member.class);
+
+            Root<Member> m = query.from(Member.class);
+
+            CriteriaQuery<Member> cq = query.select(m).where(cb.equal(m.get("name"), "kim"));
+            List<Member> resultList1 = em.createQuery(cq).getResultList();
+
+
+
 
             tx.commit();
         } catch (Exception e) {
