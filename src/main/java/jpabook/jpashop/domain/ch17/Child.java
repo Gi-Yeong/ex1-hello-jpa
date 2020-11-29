c1979e24c4e7c6881165e5fec86dbb49e682e16d
kiyoung85 <kiyoung85@gmail.com>
2020-11-29 23:59:27 +0900

    17-1 복합 키와 식별 관계 매핑 - @IdClass

---
+++ src/main/java/jpabook/jpashop/domain/ch17/Child.java
@@ -0,0 +1,17 @@
+package jpabook.jpashop.domain.ch17;
+
+import javax.persistence.*;
+
+@Entity
+public class Child {
+
+    @Id
+    private String id;
+
+    @ManyToOne
+    @JoinColumns({
+            @JoinColumn(name = "PARENT_ID1", referencedColumnName = "PARENT_ID1"),
+            @JoinColumn(name = "PARENT_ID2", referencedColumnName = "PARENT_ID2")
+    })
+    private Parent parent;
+}
---
+++ src/main/java/jpabook/jpashop/domain/ch17/Parent.java
@@ -0,0 +1,21 @@
+package jpabook.jpashop.domain.ch17;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+
+@Entity
+@IdClass(ParentId.class)
+public class Parent {
+
+    @Id
+    @Column(name = "PARENT_ID1")
+    private String id1;
+
+    @Id
+    @Column(name = "PARENT_ID2")
+    private String id2;
+
+    private String name;
+}
